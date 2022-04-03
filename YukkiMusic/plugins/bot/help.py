6dd10043cb5e136b9a48b14869e01ab614bf2939
Grey Project <95908917+grey423@users.noreply.github.com>
2022-04-03 17:17:44 +0700

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -28,12 +28,12 @@ from YukkiMusic.utils.inline.help import (help_back_markup,
 HELP_COMMAND = get_command("HELP_COMMAND")
 
 
-@app.on_message(
-    filters.command(HELP_COMMAND)
-    & filters.private
-    & ~filters.edited
-    & ~BANNED_USERS
-)
+#@app.on_message(
+#    filters.command(HELP_COMMAND)
+#    & filters.private
+#    & ~filters.edited
+#    & ~BANNED_USERS
+#)
 @app.on_callback_query(
     filters.regex("settings_back_helper") & ~BANNED_USERS
 )
